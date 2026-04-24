c255420ef27e7c8f13155ca597ab6def7c3d7f26
50134830-sudo <50134830@est.ort.edu.ar>
2026-04-24 14:28:48 -0300

    Hecho

---
+++ Assets/Scripts/Script.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Script : MonoBehaviour
+{
+    public TMP_InputField NumeroIngrezado;
+    public TextMeshProUGUI RespuestaTXT;
+    public int Respuesta = -2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RespuestaTXT.gameObject.SetActive(false);
+    }
+
+    public void Restar1()
+    {
+        int numero = int.Parse(NumeroIngrezado.text);
+
+        if (Respuesta == -2){
+            Respuesta = numero - 1;
+        }
+        else {
+            Respuesta = Respuesta - 1;
+        }
+
+        if (Respuesta > 0){
+            RespuestaTXT.text = Respuesta.ToString();
+        }
+        
+        else {
+            RespuestaTXT.text = "No se aceptan numeros menores a 1";
+        }
+        
+        RespuestaTXT.gameObject.SetActive(true);
+
+    }
+}
